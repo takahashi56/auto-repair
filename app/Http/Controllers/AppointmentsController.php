8d069ffdb8577da852c1aeaadfb9e41172d1df62
Senno <senno@Sennos-Mac.local>
2017-05-29 21:31:07 +0900

    Update

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -998,7 +998,7 @@ class AppointmentsController extends Controller
 			/* Sending email to mechanic end */
 		}
 
-		if($request->phone!=''){
+		/*if($request->phone!=''){
 			$message='Dear '.$request->name.', Thank you for booking an appointment with Gargash Autobody on '.date('d-m-y', strtotime($request->date)).' at '.date('H:i A', strtotime($request->times[0])).'. We look forward to welcoming you to a new automotive experience! Regards, Gargash Autobody';
 
 	        try{
@@ -1006,7 +1006,7 @@ class AppointmentsController extends Controller
 	    	}catch(Exception $e){
 
 	    	}
-	    }
+	    }*/
 	}
 
 	public function contact(Request $request) {
